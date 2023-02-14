d90c8e38f804f5f24bd56f286e2b957e4f504a72
mohammed <ezio.ahmed12@gmail.com>
2023-02-15 02:13:49 +0300

    add-models-views-cart-checkout

--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 
@@ -7,14 +7,39 @@ from django.shortcuts import render
 
 
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {
+        'products':products
+    }
     return render(request ,'store/store.html',context)
 
+
+
 def cart(request):
-     context = {}
-     return render(request ,'store/cart.html',context)
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order , created = Order.objects.get_or_create(customer=customer,completed=False)
+        items = order.orderitem_set.all()
+    else :
+        items =[]   
+        order = {'get_cart_total':0 , 'get_cart_items':0} 
+    context = {
+        'items':items,
+        'order':order
+    }
+    return render(request ,'store/cart.html',context)
         
 
 def checkout(request):
-    context ={}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order , created = Order.objects.get_or_create(customer=customer,completed=False)
+        items = order.orderitem_set.all()
+    else :
+        items =[]   
+        order = {'get_cart_total':0 , 'get_cart_items':0} 
+    context = {
+        'items':items,
+        'order':order
+    }
     return render(request ,'store/checkout.html',context)
